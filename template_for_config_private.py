522601470ae1b51cba48986e5cc5811cee70f4b6
luigisaetta <luigi.saetta@gmail.com>
2024-03-24 17:55:23 +0100

    added template for config_private.py

---
+++ template_for_config_private.py
@@ -0,0 +1,13 @@
+# to connect to Oracle DB EE in oraseemea
+DB_USER = "vector"
+DB_PWD = "xyz"
+DB_HOST_IP = "x.y.z.k"
+DB_SERVICE = "orclpdb1"
+
+# for GenAI Embeddings e GenAI command
+COMPARTMENT_OCID = "your ocid"
+ENDPOINT = "https://inference.generativeai.us-chicago-1.oci.oraclecloud.com"
+
+# if we want to use other LLM
+MISTRAL_API_KEY = ""
+COHERE_API_KEY = ""
